862cc30f3cb9c9def5506790f92475670a8093cc
eldisnex <ulest20@gmail.com>
2024-10-14 09:46:19 -0300

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Xprience.Models;
+
+namespace Xprience.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    // ---- Directorios ----
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Signup()
+    {
+        return View();
+    }
+
+    public IActionResult Inspiration()
+    {
+        return View();
+    }
+
+    public IActionResult Explore()
+    {
+        return View();
+    }
+
+    public IActionResult Calendar()
+    {
+        return View();
+    }
+
+    public IActionResult CreatePlan()
+    {
+        return View();
+    }
+
+    public IActionResult ReceivePlan()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
