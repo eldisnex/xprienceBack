8669d5d60d093cd4f9b55c9974ed6c12b88f63ef
eldisnex <ulest20@gmail.com>
2024-11-07 07:11:48 -0300

    Api empezada

---
+++ Models/Api.cs
@@ -0,0 +1,29 @@
+using RestSharp;
+public class Api
+{
+    string baseUrl = "https://api.foursquare.com/v3/places/";
+    string authorization = "fsq3yeHpUcjL2qJBwbXjnEkJ629BT1E38hjFCxQN/M7ujx4=";
+    RestRequest request;
+    public Api()
+    {
+        request = new RestRequest("");
+        request.AddHeader("accept", "application/json");
+        request.AddHeader("Authorization", authorization);
+    }
+
+    public async Task<string> makeRequest(string latitude, string longitude, string query)
+    {
+        RestClientOptions options = new RestClientOptions(baseUrl + $"search?ll={latitude},{longitude}&query={query}");
+        RestClient client = new RestClient(options);
+        var response = await client.GetAsync(request);
+        return response.Content;
+    }
+
+    public async Task<string> getImage(string id)
+    {
+        RestClientOptions options = new RestClientOptions(baseUrl + $"{id}/photos");
+        RestClient client = new RestClient(options);
+        var response = await client.GetAsync(request);
+        return response.Content;
+    }
+}
