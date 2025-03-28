52060f962c25e860c9ae15e00ca7188c425bc72c
BRIAN GABRIEL HUBERMAN <48179712@ort>
2025-03-28 12:13:02 -0300

    avances color

--- obj/Debug/net8.0/Xprience.AssemblyInfo.cs
+++ obj/Debug/net8.0/Xprience.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xprience")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e1fc8ad10fde13a39a4ca214695c0c7b3e323f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b99232662efc345ee4c068f574c2f4ce7b0df299")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xprience")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xprience")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/night.js
+++ wwwroot/js/night.js
@@ -1,20 +1,19 @@
-let boton
-boton = document.getElementById("BotonOscuro")
+let boton;
+boton = document.getElementById("BotonOscuro");
 
-boton.addEventListener("click", ()=>{
+let night = true;
 
-    console.log("entre")
-    document.documentElement.style.setProperty('--color1', '#000000');
-    elementos.forEach(elemento => {
-        
-        if (getComputedStyle(elemento).color === 'rgb(0, 0, 0)') {
-            elemento.style.color = 'white'; 
-        }
+
+boton.addEventListener("click", () => {
+    console.log("entre");
+    if (!night) document.documentElement.style.setProperty("--color1", "#000000");
+    else document.documentElement.style.setProperty("--color1", "#f5f5f5");
+    elementos.forEach((elemento) => {
+      if (getComputedStyle(elemento).color === "rgb(0, 0, 0)") {
+        elemento.style.color = "white";
+      }
     });
-}) 
-
-
+  night = !night;
+}); 
 
 const elementos = document.querySelectorAll("p, h1, h2,a");
-
-
